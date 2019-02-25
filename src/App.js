f2660b370c1e38bcc082be8af39d8cac26c62464
colinyb <cry4@uw.edu>
2019-02-25 00:09:46 -0800

    hard-coded fix (needs addressing in the future)

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             <Button onClick= {() => {
             }} className="Button" variant="contained" style={{margin: 10, padding: 10}} color="primary">Transcripts</Button>
             <Button onClick= {() => {
-            }} className="CTA" variant="contained" style={{margin: 5, padding: 5}} color="default">Sign Up</Button>
+            }} className="CTA" variant="contained" style={{margin: 5, padding: 5, marginLeft: 750}} color="default">Sign Up</Button>
           </header>
         </div>
         <div className="imgholder">

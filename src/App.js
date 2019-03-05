54110d7b5967385457b3558fe0a064fea2a54bf4
Nikita <nikita1020@gmail.com>
2019-03-04 16:27:52 -0800

    Implimenting Appbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Button from '@material-ui/core/Button';
 import './App.css';
+import AppHeader from './components/AppHeader.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="Header">
-          <header className="Toolbar">
-            <img src={logo} className="App-logo" style={{ marginLeft: 0 }} alt="logo" />
-            <Button onClick={() => { }} className="Button" variant="contained" style={{ margin: 10, padding: 10 }} color="primary">Voter Issues</Button>
-            <Button onClick={() => { }} className="Button" variant="contained" style={{ margin: 10, padding: 10 }} color="primary">Statistics</Button>
-            <Button onClick={() => { }} className="Button" variant="contained" style={{ margin: 10, padding: 10 }} color="primary">Transcripts</Button>
-            <Button onClick={() => { }} className="CTA" variant="contained" style={{ margin: 5, padding: 5, marginLeft: "auto", marginRight: 10 }} color="secondary">Sign Up</Button>
-          </header>
-        </div>
+        <AppHeader />
         <div className="parentContainer">
           <div className="containerLeft">
             <div className="img-left">

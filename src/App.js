170a35f82572dd52698011e441b245b84caff4f0
Mario Paolini <trepaolini@gmail.com>
2019-03-05 01:00:01 -0800

    router kinda works now

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import React, { Component } from 'react';
-import './App.css';
-import AppHeader from './components/AppHeader.js';
-import { BrowserRouter, Route } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Home from './Home.js';
-import Statistics from './Statistics.js';
-import VoterIssues from './VoterIssues.js';
+import React, { Component } from "react";
+import "./App.css";
+import AppHeader from "./components/AppHeader.js";
+import { BrowserRouter, Route } from "react-router-dom";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Home from "./Home.js";
+import Statistics from "./Statistics.js";
+import VoterIssues from "./VoterIssues.js";
 
 const styles = theme => ({
   parentContainer: {
     mt: 100
   },
   button: {
-    margin: theme.spacing.unit,
+    margin: theme.spacing.unit
   },
   input: {
-    display: 'none',
-  },
-})
+    display: "none"
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <BrowserRouter basename={process.env.PUBLIC_URL}>
-          <AppHeader />
-          <Route exact path="/" component={Home} />
-          <Route path="/statistics" component={Statistics} />
-          <Route path="/voterissues" component={VoterIssues} />
+          <div>
+            <AppHeader />
+            <Route exact path="/" component={Home} />
+            <Route path="/statistics" component={Statistics} />
+            <Route path="/voterissues" component={VoterIssues} />
+          </div>
         </BrowserRouter>
       </div>
     );
--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,69 +1,95 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import { fade } from '@material-ui/core/styles/colorManipulator';
-import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
-import Home from '../App.js';
-import VoterIssues from '../VoterIssues.js';
-import Statistics from '../Statistics.js';
+import Home from "../App.js";
+import VoterIssues from "../VoterIssues.js";
+import Statistics from "../Statistics.js";
 
 const styles = theme => ({
-    root: {
-      width: '100%',
-      boxShadow: '0 100px 100px 100px rgba(0, 0, 0, 0)',
-      zIndex:'100',
-      position: 'relative'
-    },
-    grow: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginLeft: -12,
-      marginRight: 20,
-    },
-    title: {
-      display: 'none',
-      [theme.breakpoints.up('sm')]: {
-        display: 'block',
-      },
-    },
-  });
-  
-  function ButtonAppBar(props) {
-    const { classes } = props;
-    return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <div className={classes.root}>
-          <AppBar color="primary" styles="root">
-            <Toolbar>
-              <Typography onClick={() => {this.props.router.push('/')}} className={classes.title} variant="h6" color="inherit" noWrap>
-                VoterCentric
-              </Typography>
-              <div className={classes.grow} />
-              <Button onClick={()=>{}} color="inherit" style={{margin: 10, padding: 10}}><Link to="/voterissues">Voter Issues</Link></Button>
-              <Button onClick={()=>{}} color="inherit" style={{margin: 10, padding: 10}}><Link to="/statistics">Statistics</Link></Button>
-              <Button onClick={()=>{}} color="inherit" style={{margin: 10, padding: 10}}><Link to="/transcripts">Transcripts</Link></Button>
-            </Toolbar>
-          </AppBar>
-        </div>
-        <Route exact path="/" component={Home} />
-        <Route path="/statistics" component={Statistics} />
-        <Route path="/voterissues" component={VoterIssues} />
-        <Route path="/transcripts" component={VoterIssues} />
-      </BrowserRouter>
-    );
+  root: {
+    width: "100%",
+    boxShadow: "0 100px 100px 100px rgba(0, 0, 0, 0)",
+    zIndex: "100",
+    position: "relative"
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
+    }
   }
-  
-  AppBar.propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
-  
-  export default withStyles(styles)(ButtonAppBar);
+});
+
+function ButtonAppBar(props) {
+  const { classes } = props;
+  return (
+    // <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <div className={classes.root}>
+      <AppBar color="primary" styles="root">
+        <Toolbar>
+          <Typography
+            onClick={() => {
+              this.props.router.push("/");
+            }}
+            className={classes.title}
+            variant="h6"
+            color="inherit"
+            noWrap
+          >
+            VoterCentric
+          </Typography>
+          <div className={classes.grow} />
+          <Button
+            onClick={() => {}}
+            color="inherit"
+            style={{ margin: 10, padding: 10 }}
+          >
+            <Link to="/voterissues">Voter Issues</Link>
+          </Button>
+          <Button
+            onClick={() => {}}
+            color="inherit"
+            style={{ margin: 10, padding: 10 }}
+          >
+            <Link to="/statistics">Statistics</Link>
+          </Button>
+          <Button
+            onClick={() => {}}
+            color="inherit"
+            style={{ margin: 10, padding: 10 }}
+          >
+            <Link to="/transcripts">Transcripts</Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+    // <Route exact path="/" component={Home} />
+    // <Route path="/statistics" component={Statistics} />
+    // <Route path="/voterissues" component={VoterIssues} />
+    // <Route path="/transcripts" component={VoterIssues} />
+    // </BrowserRouter>
+  );
+}
+
+AppBar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(ButtonAppBar);
